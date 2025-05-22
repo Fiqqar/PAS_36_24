5704d16f4ec941954c0a3fabb9a1d55006b1c61a
detarune <learnbygoogling@gmail.com>
2025-05-22 10:01:49 +0700

    feat: [AMIR] Create Interface For Match Schedule

---
+++ app/src/main/java/com/example/pas_semestergenap/ApiService/Interface/MatchScheduleInterface.java
@@ -0,0 +1,11 @@
+package com.example.pas_semestergenap.ApiService.Interface;
+
+import com.example.pas_semestergenap.ApiService.TimResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MatchScheduleInterface  {
+    @GET("/api/v1/json/3/eventsround.php?id=4328&r=38&s=2024-2025")
+    Call<TimResponse> getAllTeams();
+}
---
+++ app/src/main/java/com/example/pas_semestergenap/ApiService/MatchScheduleModel.java
@@ -0,0 +1,39 @@
+package com.example.pas_semestergenap.ApiService;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MatchScheduleModel {
+    @SerializedName("strEvent")
+    private String namaEvent;
+
+    @SerializedName("dateEvent")
+    private String dateEvent;
+
+    @SerializedName("strHomeTeamBadge")
+    private String homeTeamLogo;
+
+    @SerializedName("strAwayTeamBadge")
+    private String awayTeamLogo;
+
+
+
+    public MatchScheduleModel(String namaEvent, String dateEvent, String homeTeamLogo, String awayTeamlogo) {
+        this.namaEvent = namaEvent;
+        this.dateEvent = dateEvent;
+        this.homeTeamLogo = homeTeamLogo;
+        this.awayTeamLogo = awayTeamlogo;
+    }
+
+    public String getNamaClub() {return namaEvent;}
+
+    public String getDateEvent() {
+        return dateEvent;
+    }
+
+    public String getHomeTeamLogo() {
+        return homeTeamLogo;
+    }
+    public String getAwayTeamLogo() {
+        return awayTeamLogo;
+    }
+}
